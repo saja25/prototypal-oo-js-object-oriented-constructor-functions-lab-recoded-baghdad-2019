c32d8ff51dd60b12da5b82b24cf96551b1154496
saja amer <sajaamer86@gmail.com>
2019-10-16 14:12:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function Scooter(year,color,model) {
+  this.year=year;
+  this.color=color;
+  this.model=model;
+}
+function Driver (name,age,experience ) {
+  this.age=age;
+  this.name=name;
+  this.experience=experience;
+}
+function PickupLocation (address ,city ) {
+  this.address =address ;
+  this.city =city ;
+}
