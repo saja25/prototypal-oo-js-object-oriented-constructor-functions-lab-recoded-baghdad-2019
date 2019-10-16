fcd872bb6585bfc258c1487b1d46eacb4f06bdae
saja amer <sajaamer86@gmail.com>
2019-10-16 14:10:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function Scooter(year,color,model) {
   this.color=color;
   this.model=model;
 }
-function Driver (name,age,) {
+function Driver (name,age,experience ) {
   this.year=year;
   this.color=color;
   this.model=model;
